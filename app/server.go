be75b14c88666e28bbb0e0e86f4cd26afa25a47a
lailiqiang <lailiqiang@smartpos.top>
2022-11-15 16:26:13 +0800

    git commit -m "pass 4st stage" # any msg

--- app/server.go
+++ app/server.go
@@ -15,12 +15,19 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	defer l.Close()
 
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	for {
 		buf := make([]byte, 1024)
